059ae02dc35dfa5208a583d1b6db7ba05fdd7ecd
Daniel Gracia <daniel.santos@redealumni.com>
2014-11-22 20:17:41 -0200

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+u = User.new name: "Admin", email: "admin@sharedunifesp.com", password: "abc123", admin: true
+u.save! validate: false
