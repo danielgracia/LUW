447915a6c4bf2fb1a8f653b330463b801f1019b7
Daniel Gracia <daniel.santos@redealumni.com>
2014-11-26 21:00:58 -0200

    Create topics

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -16,17 +16,35 @@ class ContentsController < ApplicationController
   end
 
   def create
+    @content = current_user.contents.create(content_params)
+    if @content.errors.blank?
+      render :show
+    else
+      render :new, alert: "Erros encontrados!"
+    end
+  end
+
+  def show
+    @content = Content.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    @content = current_user.contents.find_by(id: params[:id])
+    if @content.blank?
+      redirect_to :root, alert: "RÁQUIO!"
+    elsif @content.errors.blank?
+      render :show, notice: "Postagem atualizada com sucesso!"
+    else
+      render :edit, alert: "Erros encontrados!"
+    end
   end
 
   protected
   def content_params
-    params.require(:user).permit(:title, :body, :raw_tags, :attachment_ul)
+    params.require(:user).permit(:title, :body, :raw_tags)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,9 @@ Rails.application.routes.draw do
   # Content screens
   get '/postagens', to: 'contents#browse'
   get '/postagens/busca', to: 'contents#search'
-  get '/postagem/nova', to: 'contents#new'
-  get '/postagem/:id', to: 'contents#show'
-  get '/postagem/:id/editar', to: 'contents#edit'
+  get '/postagem/nova', to: 'contents#new', as: :new_content
+  get '/postagem/:id', to: 'contents#show', as: :show_content
+  get '/postagem/:id/editar', to: 'contents#edit', as: :edit_content
 
   post '/postagem/criar', to: 'contents#create'
   post '/postagem/:id/salvar', to: 'contents#update'
@@ -26,6 +26,9 @@ Rails.application.routes.draw do
   # Comment inside content screens
   post '/postagem/:content_id/comentario/criar', to: 'contents#comment'
   post '/postagem/:content_id/comentario/:comment_id/deletar', to: 'contents#delete_comment'
+
+  # Tags
+  get '/sugestoes', to: 'tags#suggest'
 
   # Invite
   post '/convidar', to: 'invites#invite'
