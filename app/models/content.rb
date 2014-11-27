0b6be2bf9553dc368fdd9222b92017bab8dcec64
Daniel Gracia <daniel.santos@redealumni.com>
2014-11-26 22:35:48 -0200

    Oops

--- app/models/content.rb
+++ app/models/content.rb
@@ -4,13 +4,18 @@ class Content < ActiveRecord::Base
   attr_accessor :raw_tags
 
   belongs_to :user
+  has_many :comments, -> { order(created_at: :asc) }
   has_and_belongs_to_many :tags
 
   before_save :process_tags
 
   def process_tags
-    self.tags = self.raw_tags.split(",").map do |tag|
-      Tag.find_or_create_by(body: tag)
+    if self.raw_tags.present?
+      self.tags = self.raw_tags.split(",").map do |tag|
+        Tag.find_or_create_by(body: tag)
+      end
+    else
+      self.tags = []
     end
   end
 
