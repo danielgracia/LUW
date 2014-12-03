158078a0443fcd1955f63447d8c900ade86dfec1
Daniel Gracia <daniel.santos@redealumni.com>
2014-12-02 23:34:02 -0200

    Hint

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -2,6 +2,7 @@ class InvitesController < ApplicationController
   before_action :require_user
 
   def invite
+    p params[:email]
     InviteMailer.send_invite(current_user, params[:email]).deliver
     head :ok
   end
